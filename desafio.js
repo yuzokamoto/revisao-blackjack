b0a3308bdb9172c03818fda18da514f6a23f8372
yuzo <yuzokamoto@gmail.com>
2022-11-03 15:37:33 -0300

    feat: desafio / sortear cartas sem ases repetidos

--- desafio.js
+++ desafio.js
@@ -1,13 +1,46 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.clear()
 
+const isJogoAtivo = confirm("Quer iniciar uma nova rodada?")
+
+if (isJogoAtivo) {
+    console.log("Boas vindas ao jogo de blackjack!")
+
+    // ENQUANTO AS DUAS CARTAS FOREM IGUAIS À ASES, SORTEIE NOVAMENTE
+
+    function sorteiaCartas() {
+        let primeiraCarta = comprarCarta()
+        let segundaCarta = comprarCarta()
+
+        while (
+            primeiraCarta.texto.includes("A")
+            && segundaCarta.texto.includes("A")
+        ) {
+            primeiraCarta = comprarCarta()
+            segundaCarta = comprarCarta()
+        }
+
+        return [primeiraCarta, segundaCarta]
+    }
+
+    const cartasJogador = sorteiaCartas()
+
+    const totalJogador = cartasJogador[0].valor + cartasJogador[1].valor
+
+    const cartasComputador = sorteiaCartas()
+
+    const totalComputador = cartasComputador[0].valor + cartasComputador[1].valor
+
+    console.log(`Jogador: cartas - ${cartasJogador[0].texto} ${cartasJogador[1].texto} - pontuação = ${totalJogador}`)
+    console.log(`Computador: cartas - ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação = ${totalComputador}`)
+
+    if (totalJogador > totalComputador) {
+        console.log("Jogador venceu!")
+    } else if (totalJogador < totalComputador) {
+        console.log("Computador venceu!")
+    } else {
+        console.log("Empate!")
+    }
+
+} else {
+    console.log("Jogo acabou.")
+}
